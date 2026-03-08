659e1c1bfe31ab4fccf3dc0e537f5922f0cc626d
MCA25104 <ibrahimsolkar0@gmail.com>
2026-03-08 15:41:19 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,9 +1,6 @@
 public class code {
     public static void main (String [] args) {
-        System.out.println("Successfull Pulled Java file and Executed it.");
-         System.out.println("Webhook Checker.");
-        System.out.println("Webhook Checker 2.");
-        System.out.println("Webhook Checker 3.");
-        System.out.println("Webhook Checker 55551.");
+        System.out.println("Hello Jenkins");
+        System.out.println("Code Modified");
     }
 }
